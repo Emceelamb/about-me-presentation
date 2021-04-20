ccf0c8992ed6784dedad559edbb83e9a99633131
emceelamb <mlam@berkeley.edu>
2021-04-19 23:38:07 -0400

    first commit

---
+++ site.config.js
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: `Mark Lam`,
+  title: `An introduction`,
+  date: `20 April 21`,
+  author: {
+    url: `https://markofthelam.com`,
+  },
+}
+
+export default siteConfig
